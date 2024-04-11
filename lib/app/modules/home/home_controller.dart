26876534dce86668c71b14d7e54051481aac384b
Таня <tanavolosuk92@gmail.com>
2024-04-11 19:34:40 +0500

    сделала ячейки с картинками

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,23 +1,27 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  List<String> images = [
+    'https://cm.samokat.ru/processed/category/40c493a9-f765-407e-aa49-8eda265f1054.jpg',
+    'https://cm.samokat.ru/processed/category/0cf2e5af-97a9-4e7e-bdac-95c294cde41a.jpg',
+    'https://cm.samokat.ru/processed/category/86885db4-6da3-4b27-8f15-bd13438f3157.jpg',
+    'https://cm.samokat.ru/processed/category/2577cd43-edb8-494a-a114-9e6b1f2848a4.jpg',
+    'https://cm.samokat.ru/processed/category/ccda71f7-8257-42bb-98b4-0381d1b7ae25.jpg',
+    'https://cm.samokat.ru/processed/category/88a5fa26-8011-415c-8533-0389190ac964.jpg',
+    'https://cm.samokat.ru/processed/category/384a1d50-915b-4c4b-9a27-b6103ced82ca.jpg',
+    'https://cm.samokat.ru/processed/category/9ea5ad9b-0682-4462-92c5-2f19fa766a1f.jpg',
+    'https://cm.samokat.ru/processed/category/480f1901-a299-4e97-b4ad-428ab972d314.jpg',
+  ];
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
+  List<String> texts = [
+    'На завтрак',
+    'Сытный обед',
+    'Для уютного\nвечера',
+    'На перекус',
+    'Можно в пост',
+    'Порадовать\nсебя весной',
+    'Хочется\nсладкого',
+    'Сейчас сезон',
+    'Из других\nстран',
+  ];
 }
--- lib/app/modules/home/home_view.dart
+++ lib/app/modules/home/home_view.dart
@@ -4,6 +4,7 @@ import 'home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,22 +17,23 @@ class HomeView extends GetView<HomeController> {
               Container(
                 height: 40,
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(
-                      14.0), // Задаем радиус скругления краев
+                  borderRadius: BorderRadius.circular(14.0),
                   child: const TextField(
                     decoration: InputDecoration(
                       filled: true,
-                      fillColor: Color.fromRGBO(
-                          242, 242, 242, 1), // Цвет фона текстового поля
+                      fillColor: Color.fromRGBO(242, 242, 242, 1),
                       prefixIcon: Icon(
                         Icons.search,
-                        size: 20,
+                        size: 18,
                         color: Color.fromRGBO(64, 64, 64, 1),
                       ),
+                      // prefixIconConstraints:
+                      //     BoxConstraints(minWidth: 10, minHeight: 100),
                       hintText: 'Искать в Самокате',
                       hintStyle: TextStyle(
-                          fontSize: 13.5,
-                          color: Color.fromRGBO(166, 166, 166, 1)),
+                        fontSize: 13,
+                        color: Color.fromRGBO(166, 166, 166, 1),
+                      ),
                       suffixIcon: Row(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -40,22 +42,27 @@ class HomeView extends GetView<HomeController> {
                               Text(
                                 '|',
                                 style: TextStyle(
-                                    fontSize: 20,
-                                    color: Color.fromRGBO(198, 194, 194, 1)),
+                                  fontSize: 20,
+                                  color: Color.fromRGBO(198, 194, 194, 1),
+                                ),
                               ),
                               SizedBox(
                                 width: 10,
                               ),
-                              Icon(Icons.percent_rounded, size: 18,),
+                              Icon(
+                                Icons.percent_rounded,
+                                size: 16,
+                              ),
                               SizedBox(
                                 width: 3,
                               ),
                               Text(
                                 'Скидки',
                                 style: TextStyle(
-                                    color: Color.fromRGBO(64, 64, 64, 1),
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500),
+                                  color: Color.fromRGBO(64, 64, 64, 1),
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w700,
+                                ),
                               ),
                               SizedBox(
                                 width: 10,
@@ -65,13 +72,106 @@ class HomeView extends GetView<HomeController> {
                         ],
                       ),
                       border: InputBorder.none,
-                      contentPadding:
-                          EdgeInsets.all(5.0), // Отступы внутри текстового поля
+                      contentPadding: EdgeInsets.all(5.0),
                     ),
                     textAlignVertical: TextAlignVertical.top,
                   ),
                 ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              GridView.builder(
+                shrinkWrap: true,
+                gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 6.0,
+                  crossAxisSpacing: 6.0,
+                ),
+                itemCount: controller.images.length,
+                itemBuilder: (BuildContext context, int index) {
+                  var images = controller.images;
+                  var texts = controller.texts;
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(14.0),
+                    child: Container(
+                      color: const Color.fromRGBO(242, 242, 242, 1),
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.network(
+                  images[index],
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+                          Positioned(
+                            top: 4.0,
+                            left: 8.0,
+                            child: Text(
+                              texts[index],
+                              style: TextStyle(
+                                color: Color.fromRGBO(64, 64, 64, 1),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Выгодная полка',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: Color.fromRGBO(64, 64, 64, 1),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        'Больше',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12,
+                          color: Color.fromRGBO(64, 64, 64, 1),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      MaterialButton(
+                        onPressed: () {},
+                        color: Color.fromRGBO(242, 242, 242, 1),
+                        padding: EdgeInsets.all(8),
+                        shape: CircleBorder(),
+                        height: 20,
+                        minWidth: 20,
+                        child: Text(
+                          '>',
+                          style: TextStyle(
+                              fontSize: 10,
+                              color: Color.fromRGBO(64, 64, 64, 1),
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               )
+              
             ],
           ),
         ),
